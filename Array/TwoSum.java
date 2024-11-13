118892a901d3d79773782d453f5604e585453742
toshit-dh <extratdh@gmail.com>
2024-11-13 10:17:47 +0530

    Question Solved

---
+++ Array/TwoSum.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int m = nums.length;
+        for (int i = 0; i < m; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{3, 3}, 6)));
+    }
+}
